ab4784df4a6713c3e5d87d3ea2c54aa14e817c46
fatima-yar <163354495+fatima-yar@users.noreply.github.com>
2024-07-10 17:14:01 +1200

    deleted base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,24 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://fatima-yar.github.io',
-  base: 'my-repo',
 })
+
+// import { defineConfig } from 'astro/config'
+// import tailwind from '@astrojs/tailwind'
+
+// const SERVER_PORT = 3000
+// const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+// const LIVE_URL = 'https://fatima-yar.github.io'
+// let BASE_URL = LOCALHOST_URL
+// if (isBuild) {
+//   BASE_URL = LIVE_URL
+// }
+
+// // https://astro.build/config
+// export default defineConfig({
+//   server: { port: SERVER_PORT },
+//   site: BASE_URL,
+//   integrations: [tailwind()],
+//   site: 'https://fatima-yar.github.io',
+//   base: '/docs',
+// })
