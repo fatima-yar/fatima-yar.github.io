d85dccf80449d77a2cbcdfef4109234d93c1a02d
fatima-yar <163354495+fatima-yar@users.noreply.github.com>
2024-07-10 17:05:02 +1200

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 
-const SERVER_PORT = 3000
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
-const LIVE_URL = 'https://fatima-yar.github.io'
-let BASE_URL = LOCALHOST_URL
-if (isBuild) {
-  BASE_URL = LIVE_URL
-}
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: SERVER_PORT },
-  site: BASE_URL,
   integrations: [tailwind()],
   site: 'https://fatima-yar.github.io',
-  base: '/docs',
+  base: 'my-repo',
 })
