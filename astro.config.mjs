c94f7e21760ccb8ffef3943a9c55c7709c122a3f
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-07-11 19:55:58 +1200

    fix contactbtns

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://fatima-yar.github.io',
-})
+  integrations: [tailwind(), icon()],
+  site: 'https://fatima-yar.github.io'
+});
 
 // import { defineConfig } from 'astro/config'
 // import tailwind from '@astrojs/tailwind'
