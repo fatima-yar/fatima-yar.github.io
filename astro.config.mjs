9f49195ccf102ab5193efeee86d77294699e829a
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-07-14 22:52:34 +1200

    add Project1 in mainpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import compress from 'astro-compress';
 
-import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon(),
-    compress(),
-    (await import('astro-compress')).default(),
-  ],
-  site: 'https://fatima-yar.github.io',
-})
+  integrations: [tailwind(), icon(), compress(), (await import('astro-compress')).default()],
+  site: 'https://fatima-yar.github.io'
+});
