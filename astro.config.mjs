fd9210a893c8fe95f5333bde6f062dffc70973cc
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-04-22 17:06:42 +1200

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,7 @@ import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import compress from 'astro-compress'
 
-// https://astro.build/config
 export default defineConfig({
-  experimental: {
-    transitions: true,
-  },
-  integrations: [
-    tailwind(),
-    icon(),
-    compress(),
-    (await import('astro-compress')).default(),
-  ],
+  integrations: [tailwind(), icon(), compress()],
   site: 'https://fatima-yar.github.io',
 })
